cdf55c5f74c86f7b96f61a4afb5b44f5439a16e4
tobias57 <tobias@sthorm.io>
2022-01-26 01:55:07 -0300

    feat(contract): modified contract for new functions

--- contract.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.5.0 <0.6.0
-
-contract HelloWorld{
-
-    
-
-
-}
-
---
+++ contracts/contract.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.7.0;
+
+contract HelloBlockchain {
+
+    string[] word;
+
+    event newWord(string word);
+    event wordExist(bool exist);
+
+    mapping (string => bool) public Words;
+
+    function _addWord(string memory _name) public{
+
+        word.push(_name);
+        Words[_name] = true;
+        emit newWord(_name);
+
+
+    }
+
+    function _verifyWord(string memory _word) public returns(bool){
+
+        emit wordExist(Words[_word]);
+        return Words[_word];
+    }
+
+}
